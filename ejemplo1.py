f0c8fe5995fc1137884391aabef891486c9c85ef
Douglas Jimenez <douglasdj.code@gmail.com>
2024-11-11 21:49:11 -0300

    agregue una nueva funcion

---
+++ ejemplo1.py
@@ -0,0 +1,9 @@
+def saludando():
+    print("Hola chicos arrancamos con GIT")
+
+saludando()
+
+def sumar(n1,n2):
+    print(f"El resultado de la suma entre n1 + n2 es {n1+n2}")
+
+sumar(int(input("Ingresa el primer numero: "))), (int(input("Ingresa el segundo numero: ")))
